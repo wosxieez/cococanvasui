5d908f37c65e9c87f7b97a2bc42d0d759dcc8607
xez <xez@ipms.cn>
2022-02-22 13:43:06 +0800

    代码更新提交

--- src/cocoui/core/Application.js
+++ src/cocoui/core/Application.js
@@ -3,5 +3,13 @@ import UIComponent from '../core/UIComponent'
 export default class Application extends UIComponent {
   constructor() {
     super()
+
+    console.log('hello this is application')
+  }
+
+  createChildren() {
+    super.createChildren()
+
+    console.log('hello this is application createChildren')
   }
 }
--- src/cocoui/core/UIComponent.js
+++ src/cocoui/core/UIComponent.js
@@ -2,4 +2,9 @@ export default class UIComponent {
   constructor() {
     console.log('hello this is uicomponent')
   }
+
+  createChildren() {
+    console.log('hello this is uicomponent createChildren')
+  }
+
 }
